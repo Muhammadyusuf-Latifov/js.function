edab3de85e6485a2d29eae1821249d03f8523d08
Muhammadyusuf-Latifov <longhaochen200@gmail.com>
2025-04-28 16:17:46 +0500

    function

---
+++ js/app.js
@@ -0,0 +1,90 @@
+{
+  // m - 1
+  //   function result(a) {
+  //     return a * 2;
+  //   }
+  //   console.log(result(5));
+}
+{
+  // m - 2
+  //   function fname(ism) {
+  //     return `Salom ${ism}`;
+  //   }
+  //   console.log(fname("Akbar"));
+}
+{
+  // m - 3
+  //   function misol(n) {
+  //     if (n > 0) {
+  //       return `${n} - musbat`;
+  //     } else if (n < 0) {
+  //       return `${n} - manfiy`;
+  //     } else {
+  //       return `${n} = 0 ga teng`;
+  //     }
+  //   }
+  //   console.log(misol(33));
+  //   console.log(misol(-33));
+  //   console.log(misol(0));
+}
+{
+  // m - 4
+  //   function toq(n) {
+  //     return n % 2 === 0 ? `${n} - juft son` : `${n} - toq son`;
+  //   }
+  //   console.log(toq(2));
+  //   console.log(toq(5));
+}
+{
+  //  m - 5
+  //   function katta(a, b) {
+  //     return a > b ? `${a}` : `${b}`;
+  //   }
+  //   console.log(katta(5, 3));
+}
+{
+  //  m - 6
+  //   function loop(n) {
+  //     for (let i = 1; i <= n; i++) {
+  //       console.log(i);
+  //     }
+  //   }
+  //   loop(9);
+}
+{
+  // m - 7
+  //   function yigindi(son) {
+  //     let sum = 0;
+  //     for (let i = 0; i < son.length; i++) {
+  //       sum += son[i];
+  //     }
+  //     return sum;
+  //   }
+  //   let numbers = [1, 1, 3, 4, 4];
+  //   console.log(yigindi(numbers));
+}
+{
+  //    m - 8
+  //   function teskari(soz) {
+  //     let teskariIsm = "";
+  //     for (let i = soz.length - 1; i >= 0; i--) {
+  //       teskariIsm += soz[i];
+  //     }
+  //     return teskariIsm;
+  //   }
+  //   let ism = "laylo";
+  //   let result = teskari(ism);
+  //   console.log(result);
+}
+{
+  // m - 10
+  //   function foktaryal(a) {
+  //     let sum = 1;
+  //     for (let i = a; i >= 1; i--) {
+  //       sum *= i;
+  //     }
+  //     return sum;
+  //   }
+  //   let result = foktaryal(7);
+  //   console.log(result);
+}
